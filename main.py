518a0e8cd45ee67d87591d7d0fd0411c20086c5c
achieve_dream <wulei7217@gmail.com>
2024-09-14 14:18:25 +0800

    refactor(paddle): 将项目依赖的Python版本更新到3.10，并转换为使用paddle2onnx
    - 移除.py文件中的多余注释。
    - 调整依赖列表，包含paddle2onnx并移除未使用的依赖。
    - 从README中移除过时的依赖信息，并补充新的依赖说明。
    - 删除requirements.lock和requirements-dev.lock文件以准备同步新的依赖。
    - 在main.py中实现模型转换逻辑，确保与新依赖兼容。
    
    BREAKING CHANGE: 此调整要求Python 3.10，并依赖paddle2onnx 1.0.6版本。

--- main.py
+++ main.py
@@ -4,3 +4,28 @@
 # @Author  : ACHIEVE_DREAM
 # @File    : main.py
 # @Software: Pycharm
+
+import paddle2onnx
+from pathlib import Path
+
+
+def main():
+    input_dir = Path("input")
+    output_dir = Path("output")
+    output_dir.mkdir(exist_ok=True)
+    if not input_dir.exists():
+        raise FileNotFoundError(f"{input_dir} not exists")
+    for i in input_dir.iterdir():
+        if i.is_file():
+            continue
+        name = i.name
+        paddle2onnx.export(
+            model_file=str(i / "inference.pdmodel"),
+            params_file=str(i / "inference.pdiparams"),
+            opset_version=16,
+            save_file=f"{output_dir / name}.onnx",
+        )
+
+
+if __name__ == "__main__":
+    main()
